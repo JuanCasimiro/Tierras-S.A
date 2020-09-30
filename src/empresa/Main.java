3a2ff37c2898ea8adb5b765c00492f04b083fc3b
Juan Casimiro <juanyc434@gmail.com>
2020-09-30 18:03:17 -0300

    cambios

--- src/empresa/Main.java
+++ src/empresa/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		Camionero pepe = new Camionero("pepe");
 		Cliente juan = new Cliente();
 		Trabajo trabajo = new Trabajo(10, 20, 100000);
-		Duenio valen = new Duenio("valen");
+		Dueño valen = new Duenio("valen");
 		Empresa volpiSA = new Empresa("volpi",valen, 200, 100);
 		
 	}
